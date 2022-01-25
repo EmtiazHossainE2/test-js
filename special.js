e9835fdc601d69ba38727d658780cd4adc699af5
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-01-26 01:19:56 +0600

    variable js

---
+++ special.js
@@ -0,0 +1,5 @@
+var num1 = 0.2 ;
+var num2 = 0.3 ;
+total = num1 + num2 ; 
+total = parseFloat (total);
+console.log(total); 
--- variable.js
+++ variable.js
@@ -1,4 +1,7 @@
-var name = 'EMon';
+var name = 'Emtiaz';
 var age = 22 ;
 var birthDay = 3 ;
+var habiJabi = 'Yoyo';
 console.log(age);
+console.log(name);
+console.log(habiJabi);
