5a2db16724512c0dd2198705d42473ce4b3c02bd
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-01-26 14:47:10 +0600

    conditional check

---
+++ conditional.js
@@ -0,0 +1,14 @@
+if(3 < 5 ){
+    console.log('Yes true');
+}
+var age = 16
+if(age < 17){
+    console.log('You are child');
+}
+
+else if ( age >= 20){
+    console.log('You are adult');
+}
+else{
+    console.log('you are not adult');
+}
