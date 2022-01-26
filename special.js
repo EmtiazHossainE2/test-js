dea224572699bab8e7d095094cf731e122b74317
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-01-26 12:24:00 +0600

    toFixed js learn

--- special.js
+++ special.js
@@ -1,6 +1,8 @@
-var num1 = 0.2 ;
+var num1 = 0.285885454 ;
 var num2 = 0.3 ;
 total = num1 + num2 ; 
-total =total.toFixed(1);
-total = parseFloat(total);
-console.log(total); 
+console.log(parseFloat(total.toFixed(3))); 
+
+var num3 = "99.5";
+var num4 = 15;
+console.log(typeof (num3+num4));
