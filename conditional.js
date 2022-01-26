aa9d37d47b6a9f5c3019eb951aa3165985f2c5ae
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-01-26 14:59:46 +0600

    index file added

--- conditional.js
+++ conditional.js
@@ -1,14 +1,38 @@
 if(3 < 5 ){
     console.log('Yes true');
 }
-var age = 16
+/* var age = 16
 if(age < 17){
     console.log('You are child');
-}
+} */
 
 else if ( age >= 20){
     console.log('You are adult');
 }
 else{
     console.log('you are not adult');
+}
+
+var num = 9 
+if(num > 1){
+    if(num >= 10){
+        console.log('greater than 10');
+    }
+    else {
+        console.log(' less than 10');
+    }
+}
+var age = 65
+if(age > 18 ){
+    if(age > 30 ){
+        console.log('You are a complete man');
+    }
+    else if(age > 60 ){
+        console.log('you are an old man ');
+    }
+    else{
+        console.log('you are young');
+    }
+}else{
+    console.log('you are child');
 }
