8eb9d6e6d2c4a5be839d0176019565616d47c158
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-01-27 03:09:47 +0600

    some condition check

--- array.js
+++ array.js
@@ -1,6 +1,16 @@
-var number = [1,2,3,4,5,6,7,8,9];
-// var total = number.indexOf(0);
-// var total = number.indexOf(1);
-var total = number.indexOf(9);
-// var total = number.indexOf(10);
-console.log(total);
+// var number = [1,2,3,4,5,6,7,8,9];
+// // var total = number.indexOf(0);
+// // var total = number.indexOf(1);
+// var total = number.indexOf(9);
+// // var total = number.indexOf(10);
+// console.log(total);
+
+// var dream = ['i', 'will', 'become', 'a', 'developer', 'inshaallah'];
+// var myDream = dream.indexOf('developer');
+// console.log(myDream);
+
+arr = [3, 7, 10] ; 
+arr[1] = [3];
+console.log(arr);
+console.log(6 != 6);
+console.log(6 >= 6);
