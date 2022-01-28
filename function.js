d64e6c38f9ea8ec53b61e06572014560c7eeae93
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-01-28 23:17:22 +0600

    advanced function learn

--- function.js
+++ function.js
@@ -3,3 +3,13 @@ function firstFunction(){
     console.log('Learn Learn Learn ');
 }
 firstFunction();
+
+function samucha (price){
+    console.log(' Samucha ante dici '+ price );
+    var samuchaPrice = 10 ; 
+    var samuchaPabo = price / samuchaPrice ; 
+    console.log(samuchaPabo);
+}
+var tk = 100 ;
+var yourSamucha = samucha(tk);
+console.log('Your samucha ' + yourSamucha);
