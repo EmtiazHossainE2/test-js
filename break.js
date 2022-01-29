75875c877633b4643f25a2c65b48f903432a9917
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-01-29 17:27:53 +0600

    switch statement

---
+++ break.js
@@ -0,0 +1,27 @@
+var color = 'red ' ;
+switch (color){
+    case 'violet ' : 
+        console.log('Color is  violet ');
+        break ;
+    case 'blue ' : 
+        console.log('Color is  blue ');
+        break ;
+    case 'sky ' : 
+        console.log('Color is  sky ');
+        break ;
+    case 'green ' : 
+        console.log('Color is  green ');
+        break ;
+    case 'yellow ' : 
+        console.log('Color is  yellow ');
+        break ;
+    case 'orange ' : 
+        console.log('Color is  orange ');
+        break ;
+    case 'red ' : 
+        console.log('Color is  red ');
+        break ;
+    default : 
+        console.log('Color is Black ');
+}
+
